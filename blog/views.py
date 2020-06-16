b0bef26b6b854cf07356fe3529f132d92f190625
khalidamairi3 <khalidamairi3@gmail.com>
2020-06-16 02:54:30 -0600

    add postlist

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
+def postlist(request):
+    posts=Post.objects.filter(pub_date__lte=timezone.now()).order_by("pub_date")
+    front_end_stuff={"posts":posts}
+    return render(request,"index.html",front_end_stuff)
+
+
+
 
 # Create your views here.

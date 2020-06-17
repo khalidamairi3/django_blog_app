93ccacc07ae0860f49a90516192bfe6dd8c7e991
khalidamairi3 <khalidamairi3@gmail.com>
2020-06-16 18:50:06 -0600

    add blog details

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import postlist
+from .views import postlist,detail
 
 urlpatterns=[
-    path("",postlist,name="postlist")
+    path("",postlist,name="postlist"),
+    path("post/<int:post_id>",detail,name="detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,14 @@ def postlist(request):
     posts=Post.objects.filter(pub_date__lte=timezone.now()).order_by("pub_date")
     front_end_stuff={"posts":posts}
     return render(request,"index.html",front_end_stuff)
+def detail(request,post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+        front_end_stuff={"post":post}
+    except Exception as e:
+        raise
+    return render(request,"detail.html",front_end_stuff)
+
 
 
 

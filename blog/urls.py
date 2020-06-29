02a869205afcc0431f2db6df8a8ff7f75058c047
khalidamairi3 <khalidamairi3@gmail.com>
2020-06-29 11:12:32 -0600

    add log out functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path("",views.postlist,name="postlist"),
@@ -9,4 +9,5 @@ urlpatterns=[
     path("post/<int:post_id>/edit",views.edit,name="edit"),
     path("post-draft-list",views.post_draft_list,name="post-draft-list"),
     path("post/<int:pk>/publish",views.publish_post,name="publish_post"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="registeration/login.html"), name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
-
+from django.contrib.auth.decorators import login_required
 def postlist(request):
     posts=Post.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")
     front_end_stuff={"posts":posts}
@@ -15,21 +15,20 @@ def detail(request,post_id):
         raise
     return render(request,"detail.html",front_end_stuff)
 
-
+@login_required
 def createpost(request):
     if request.method=='POST':
         form=PostForm(request.POST)
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.pub_date=timezone.now()
             post.save()
             return redirect('detail',post_id=post.pk)
     else:
         form =PostForm()
         front_end_stuff={'form':form}
     return render(request,"post-edit.html",front_end_stuff)
-
+@login_required
 def edit(request,post_id):
     post=get_object_or_404(Post,pk=post_id)
     if request.method=='POST':
@@ -37,19 +36,18 @@ def edit(request,post_id):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.pub_date=timezone.now()
             post.save()
             return redirect('detail',post_id=post.pk)
     else:
         form =PostForm(instance=post)
         front_end_stuff={'form':form}
     return render(request,"post-edit.html",front_end_stuff)
-
+@login_required
 def post_draft_list(request):
     posts=Post.objects.filter(pub_date__isnull=True).order_by('-created_date')
     front_end_stuff={"posts":posts}
     return render(request,"post-draft-list.html",front_end_stuff)
-
+@login_required
 def publish_post(request,pk):
     post=get_object_or_404(Post,pk=pk)
     post.publish()
